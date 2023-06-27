6f171fa74d72c9d9b5286bceed2b3aa7bade1921
ConejoCaco <huanca1992mamani@gmail.com>
2023-06-27 17:24:38 -0400

    sala

--- proyecto1/Sala.cs
+++ proyecto1/Sala.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace proyecto1
 {
-	internal class Sala
+	class Sala
 	{
-	}
+		private string nombreSala;
+		private bool tieneCierre, tieneAlarma;
+		private float temperatura;
+		private int lamparas;
+
+
+        public string NombreSala
+        {
+            get { return nombreSala; }
+            set { nombreSala = value; }
+        }
+
+        public bool TieneCierre
+        {
+            get { return tieneCierre; }
+            set { tieneCierre = value; }
+        }
+
+        public bool TieneAlarma
+        {
+            get { return tieneAlarma; }
+            set { tieneAlarma = value; }
+        }
+
+        public float Temperatura
+        {
+            get { return temperatura; }
+            set { temperatura = value; }
+        }
+
+        public int Lamparas
+        {
+            get { return lamparas; }
+            set { lamparas = value; }
+        }
+
+
+    }
 }
